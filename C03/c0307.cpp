1cac58afb4a53d02959d0f24786326b0c9790a98
Br2203 <brian.quispev@gmail.com>
2025-01-10 08:12:43 -0500

    Delete C03 directory

--- C03/c0307.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::string p= "Hola";
-    std::cout<<p<<"  "<<sizeof(p)<<"B\n";
-    return 0;
-}
--- C03/c0308.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main(){
-    bool x= true;
-    bool y= false;
-    std::cout<<"Variable booleana x: "<<x<<" de tamano: "<<sizeof(x)<<"B\n";
-    std::cout<<"Variable booleana y: "<<y<<" de tamano: "<<sizeof(y)<<"B\n";
-    return 0;
-}
